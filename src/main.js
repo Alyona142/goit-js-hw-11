b66ec99fb420cfc2a96d6407d88eeec4c33dd64a
Alyona <olenapav97@gmail.com>
2024-11-01 22:01:48 +0200

    ссс

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// Імпорт бібліотек
 import { fetchImages } from './js/pixabay-api.js';
 import { renderGallery } from './js/render-functions.js';
 import SimpleLightbox from 'simplelightbox';
@@ -10,19 +9,43 @@ const API_KEY = '46806668-35f52dba5a140225900df36e0';
 const API_URL = 'https://pixabay.com/api/';
 
 const searchForm = document.querySelector('.search-form'); 
-const searchInput = document.getElementById('search-input');
+const searchInput = searchForm.querySelector('input[name="search"]'); 
 const gallery = document.querySelector('.gallery');
 const loaderContainer = document.querySelector('.loader-container'); 
 
+if (!searchForm || !searchInput || !gallery || !loaderContainer) {
+    console.error('One or more elements are not found in the DOM.');
+}
 
 const lightbox = new SimpleLightbox('.gallery a');
 
-
 function showLoader() {
-  loaderContainer.style.display = 'flex';
+  loaderContainer.style.display = 'flex'; 
 }
 
 function hideLoader() {
-  loaderContainer.style.display = 'none';
+  loaderContainer.style.display = 'none'; 
 }
 
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const query = searchInput.value.trim(); 
+  if (query.length === 0) {
+    return; 
+  }
+
+  showLoader(); 
+
+  fetchImages(query)
+    .then(images => {
+      renderGallery(gallery, images);
+      lightbox.refresh();
+      hideLoader();
+    })
+   .catch(error => {
+      iziToast.error({ title: 'Error', message: error.message }); 
+      hideLoader(); 
+      gallery.innerHTML = '';
+    });
+}); 
