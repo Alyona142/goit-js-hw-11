9b87d7cb47374e064b066b4a2ac9f8441b7ce3b0
Alyona <olenapav97@gmail.com>
2024-11-02 14:44:07 +0200

    ccc

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { fetchImages } from './js/pixabay-api.js';
 import { renderGallery } from './js/render-functions.js';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.css';
+import 'izitoast/dist/css/iziToast.css';
 
 const API_KEY = '46806668-35f52dba5a140225900df36e0';
 const API_URL = 'https://pixabay.com/api/';
@@ -13,39 +11,41 @@ const searchInput = searchForm.querySelector('input[name="search"]');
 const gallery = document.querySelector('.gallery');
 const loaderContainer = document.querySelector('.loader-container'); 
 
-if (!searchForm || !searchInput || !gallery || !loaderContainer) {
-    console.error('One or more elements are not found in the DOM.');
-}
-
 const lightbox = new SimpleLightbox('.gallery a');
 
 function showLoader() {
-  loaderContainer.style.display = 'flex'; 
+  loaderContainer.style.display = 'flex';
 }
 
 function hideLoader() {
-  loaderContainer.style.display = 'none'; 
+  loaderContainer.style.display = 'none';
 }
 
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const query = searchInput.value.trim(); 
+  const query = searchInput.value.trim();
   if (query.length === 0) {
-    return; 
+    return;
   }
 
-  showLoader(); 
-
+  showLoader();
   fetchImages(query)
     .then(images => {
-      renderGallery(gallery, images);
-      lightbox.refresh();
+
+      if (Array.isArray(images) && images.length > 0) {
+        renderGallery(gallery, images);
+        lightbox.refresh();
+      } else {
+        console.warn('Немає зображень для цього запиту');
+        iziToast.info({ title: 'Info', message: 'No images found for your search query.' });
+        gallery.innerHTML = '';
+      }
       hideLoader();
     })
-   .catch(error => {
-      iziToast.error({ title: 'Error', message: error.message }); 
-      hideLoader(); 
+    .catch(error => {
+      console.error('Помилка при завантаженні зображень:', error);
+      hideLoader();
       gallery.innerHTML = '';
     });
-}); 
+  });
